34d2734ad8136ec081b7258f4929ef708f6b0c9c
Omar Negm <omarnegm2014@gmail.com>
2022-09-21 17:34:16 +0200

    The main file

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * traffic_control.c
+ *
+ * Created: 9/8/2022 11:52:28 AM
+ * Author : omarn
+ */ 
+
+// student name: Omar Saeed Ali Ahmed Negm
+
+//#include "Application/test.h"
+#include "Application/app.h"
+int main(void)
+{ 
+    //1. establishing the project 
+	APP_init();
+    while (1) 
+    {//2. run the project
+		APP_run();
+    }
+}
+
