2cf3bc5e6630b1781b443f11d7cc5f6aa399dadc
Omar Negm <omarnegm2014@gmail.com>
2022-09-21 17:32:20 +0200

    Add files via upload
    test: Includes the test modules of the drivers

---
+++ Application/test.h
@@ -0,0 +1,23 @@
+/*
+ * test.h
+ *
+ * Created: 9/11/2022 8:21:06 PM
+ *  Author: omarn
+ */ 
+
+//NOTE: You can remove error handling of any function of any driver -if not necessary-
+//.. to have more accurate delay as required.
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+
+/*
+#include "../MCAL/DIO/dio.h"	//	DIO_main();
+#include "../MCAL/Timer/timer.h"	//	timer_main();
+#include "../MCAL/Interrupt/interrupt.h"	//	INT_main();
+#include "../ECUAL/button/button.h"	//	button_main();
+#include "../ECUAL/LED/LED.h"	//	LED_main();	*/
+
+#endif /* TEST_H_ */
